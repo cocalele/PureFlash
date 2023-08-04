a3ebc91da8a373b0abf6db16904d6d7d905c6092
LiuLele <cocalele@gmail.com>
2023-08-04 21:48:51 +0800

    upgrade to ubuntu22.04 as container base

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -38,15 +38,5 @@ assert cp -rp $PFS_BUILD/bin/pfdd .
 assert cp -rp $QEMU_BUILD/qemu-img .
 assert cp -rp $FIO_BUILD/fio .
 
-echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted
-deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted
-deb http://mirrors.aliyun.com/ubuntu/ focal universe
-deb http://mirrors.aliyun.com/ubuntu/ focal-updates universe
-deb http://mirrors.aliyun.com/ubuntu/ focal multiverse
-deb http://mirrors.aliyun.com/ubuntu/ focal-updates multiverse
-deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted
-deb http://mirrors.aliyun.com/ubuntu/ focal-security universe
-deb http://mirrors.aliyun.com/ubuntu/ focal-security multiverse" > sources.list
 
 docker build -f Dockerfile -t pureflash/pureflash:1.8.3 .
