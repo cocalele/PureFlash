7f025e76e55400b0eca222ec3d742a0db1df92be
LiuLele <cocalele@gmail.com>
2025-04-30 09:12:21 +0800

    Fix compile error

--- common/src/pf_pfdd.cpp
+++ common/src/pf_pfdd.cpp
@@ -17,7 +17,8 @@ void unexpected_exit_handler();
 #define FIX_ISAL_LINK_PROBLEM 1
 #ifdef FIX_ISAL_LINK_PROBLEM
 #include "raid.h"
-
+#include "crc.h"
+#include "crc64.h"
 #define TEST_SOURCES 16
 #define TEST_LEN     16*1024
 
@@ -25,26 +26,13 @@ void unexpected_exit_handler();
 int _fake_use_xor_gen()
 {
 
-	int i, j, should_pass, should_fail;
+	int i, j, should_fail;
 	void* buffs[TEST_SOURCES + 1];
 
-	printf("XOR example\n");
-	for (i = 0; i < TEST_SOURCES + 1; i++) {
-		void* buf;
-		if (posix_memalign(&buf, 32, TEST_LEN)) {
-			printf("alloc error: Fail");
-			return 1;
-		}
-		buffs[i] = buf;
-	}
 
-	printf("Make random data\n");
-	for (i = 0; i < TEST_SOURCES + 1; i++)
-		for (j = 0; j < TEST_LEN; j++)
-			((char*)buffs[i])[j] = rand();
-
-	printf("Generate xor parity\n");
 	xor_gen(TEST_SOURCES + 1, TEST_LEN, buffs);
+	crc32_iscsi(NULL, 0, 0);
+	crc64_rocksoft_refl(0, NULL, 0);
 	return 0;
 }
 #endif
