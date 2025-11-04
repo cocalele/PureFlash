6f4b837673aaac99e54317f8897725bc73ac05ef
root <280965257@qq.com>
2025-11-04 13:55:51 +0800

    fix: consider advice of qiyuanzhi, pfstore volume meta_ver equal to pfconductor, not primary replica can not be read or write

--- pfs/src/pf_dispatcher.cpp
+++ pfs/src/pf_dispatcher.cpp
@@ -37,11 +37,11 @@ int PfDispatcher::prepare_volume(PfVolume* vol)
 	if (pos != opened_volumes.end())
 	{
 		PfVolume* old_v = pos->second;
-		//if(old_v->meta_ver >= vol->meta_ver) {
-		//	S5LOG_WARN("Not update volume in dispatcher:%d, vol:%s, whose meta_ver:%d new meta_ver:%d",
-		//	  disp_index, vol->name, old_v->meta_ver, vol->meta_ver);
-		//	return 0;
-		//}
+		if(old_v->meta_ver > vol->meta_ver) {
+			S5LOG_WARN("Not update volume in dispatcher:%d, vol:%s, whose meta_ver:%d new meta_ver:%d",
+			  disp_index, vol->name, old_v->meta_ver, vol->meta_ver);
+			return 0;
+		}
 
 		(*old_v) = std::move(*vol);
 	} else {
